fec4c258f39c43b830c3e48576775c02199cf44b
Miranda Aliashvili <miranda.aliashvili@gmail.com>
2026-09-24 21:24:06 +0400

    Create server.js

---
+++ Build a Submission Form/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import apiRouter from "./routes/api.routes.js";
+import { notFoundHandler, finalErrorHandler } from "./middleware/error.middleware.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use("/api", apiRouter);
+app.use(notFoundHandler);
+app.use(finalErrorHandler);
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000");
+});v
